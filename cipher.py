d4d3c6f35ff707cb6495fedceda4a655e68c1b27
DecodingSanskrit <139328495+DecodingSanskrit@users.noreply.github.com>
2023-07-12 03:28:44 -0700

    update python

--- cipher.py
+++ cipher.py
@@ -9,6 +9,7 @@ class cipher:
     all_letters = string.ascii_letters
 
     dictionary = {}
+    dict2 = {}    
 
     def __init__(self, keyvalues):
         if all_letters.len
@@ -17,6 +18,9 @@ class cipher:
     def __init__(self, key):
         for i in range(len(all_letters)):
                 dictionary[all_letters[i]] = all_letters[(i+key)%len(all_letters)]
+        
+        for i in range(len(all_letters)):
+            dict2[all_letters[i]] = all_letters[(i-key)%(len(all_letters))]
 
     def encrypt(textToEncrypt):
         for char in plain_txt:
@@ -33,7 +37,7 @@ class cipher:
     def decrypt(textToDecrypt):
         for char in cipher_txt:
             if char in all_letters:
-                temp = dictionary[char]
+                temp = dict2[char]
                 decrypt_txt.append(temp)
             else:
                 temp = char
